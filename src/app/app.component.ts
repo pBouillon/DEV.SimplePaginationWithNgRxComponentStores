67dc3369d26c3ab4d10134545c722e6bd7d92ddc
pBouillon <pro.pierre.bouillon@protonmail.com>
2023-02-27 18:35:07 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `
+    <h1>Simple pagination with NgRx component stores</h1>
+  `,
+  styles: []
+})
+export class AppComponent {
+}
